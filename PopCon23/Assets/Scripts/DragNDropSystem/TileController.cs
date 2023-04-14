34a4ace34fcd2239b2cf0e0be71aa6b996ee52d7
lkurker <kurkerlogan@gmail.com>
2023-04-14 11:09:02 -0400

    Drag and drop system works now

---
+++ PopCon23/Assets/Scripts/DragNDropSystem/TileController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TileController : MonoBehaviour
+{
+
+    //this variable is used to determine if the tile is currently empty or not. Since some tiles will be filled by default, I am setting this variable as public so that we can change it if need be
+    public bool isEmpty = true;
+
+}
